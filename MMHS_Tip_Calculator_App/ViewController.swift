71361d74b73ea3351eb891c891ff15323aeded19
vikdenic <vik.denic@gmail.com>
2014-07-23 11:53:33 -0500

    Set up storyboard

--- MMHS_Tip_Calculator_App/ViewController.swift
+++ MMHS_Tip_Calculator_App/ViewController.swift
@@ -8,18 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
                             
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var billAmountTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //Delegate Method
+    @IBAction func onBillAmountTextFieldEdited(sender: AnyObject)
+    {
+        
     }
-
 
 }
 
