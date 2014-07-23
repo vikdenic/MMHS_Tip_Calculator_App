0ef12b5a1475ca014e35aff192edbc36aedf3264
vikdenic <vik.denic@gmail.com>
2014-07-23 10:40:55 -0500

    Initial Commit

---
+++ MMHS_Tip_Calculator_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MMHS_Tip_Calculator_App
+//
+//  Created by Vik Denic on 7/23/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
