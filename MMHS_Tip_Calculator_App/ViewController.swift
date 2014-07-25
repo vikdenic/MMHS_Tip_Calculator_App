7ee6760d58347a435aa73b7643458b8479f598e5
vikdenic <vik.denic@gmail.com>
2014-07-25 09:16:16 -0500

    Logic implemented. Needs button to change to selected image. Needs layout fixed

--- MMHS_Tip_Calculator_App/ViewController.swift
+++ MMHS_Tip_Calculator_App/ViewController.swift
@@ -12,17 +12,26 @@ class ViewController: UIViewController, UITextFieldDelegate {
                             
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var billAmountTextField: UITextField!
+
+    var tipPercent : Double!
     var tipAmount : Double!
 
-    override func viewDidLoad() {
+    @IBOutlet weak var tenButton: UIButton!
+    @IBOutlet weak var fifteenButton: UIButton!
+    @IBOutlet weak var twentyButton: UIButton!
+    @IBOutlet weak var twentyFiveButton: UIButton!
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        tipPercent = 0.15
     }
 
     //Helper method to calculate tip
     func calculateTip()
     {
         var billAmount = billAmountTextField.text.bridgeToObjectiveC().doubleValue
-        tipAmount = billAmount * 0.15
+        tipAmount = billAmount * tipPercent
 
         //Format string to display dollar signs and 2 decimal places
         var tipString = NSString(format:"$%.2f", tipAmount)
@@ -42,5 +51,47 @@ class ViewController: UIViewController, UITextFieldDelegate {
     {
         view.endEditing(true)
     }
+
+    //button actions
+    @IBAction func onTenButtonPressed(sender: UIButton)
+    {
+//        resetAllButtons()
+     tenButton.imageView.image = UIImage(named: "10_selected_image")
+        tipPercent = 0.10
+        calculateTip()
+    }
+
+    @IBAction func onFifteenButtonPressed(sender: UIButton)
+    {
+//        resetAllButtons()
+        fifteenButton.imageView.image = UIImage(named: "15_selected_image")
+        tipPercent = 0.15
+        calculateTip()
+    }
+
+    @IBAction func onTwentyButtonPressed(sender: UIButton)
+    {
+//        resetAllButtons()
+        twentyButton.imageView.image = UIImage(named: "20_selected_image")
+        tipPercent = 0.20
+        calculateTip()
+    }
+
+    @IBAction func onTwentyFiveButtonPressed(sender: UIButton)
+    {
+//        resetAllButtons()
+        twentyFiveButton.imageView.image = UIImage(named: "25_selected_image")
+        tipPercent = 0.25
+        calculateTip()
+    }
+
+//    //helper method that resets all buttons to unselected image
+//    func resetAllButtons()
+//    {
+//        tenButton.imageView.image = UIImage(named: "ten_unselected_image")
+//        fifteenButton.imageView.image = UIImage(named: "fifteen_unselected_image")
+//        twentyButton.imageView.image = UIImage(named: "twenty_unselected_image")
+//        twentyFiveButton.imageView.image = UIImage(named: "twentyFive_unselected_image")
+//    }
 }
 
